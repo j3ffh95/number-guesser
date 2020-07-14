7a1a616bd4419ab99856518554d473a7ccce1bf6
j3ffh95 <j3ffh95@gmail.com>
2020-07-14 17:06:50 -0400

    Added random number function

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI Elements
@@ -83,6 +83,11 @@ function gameOver(won, msg) {
   // Play again
   guessBtn.value = "Play again";
   guessBtn.className += "play-again";
+}
+
+// Get random number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Set message function
